07cf8f7736b24d399899d46e33de58acefd31947
Rosie Hoyem <rosiehoyem@gmail.com>
2014-01-06 16:39:17 -0600

    tweaked views

--- spec/feature_helper.rb
+++ spec/feature_helper.rb
@@ -1,2 +1,3 @@
 require 'spec_helper'
 require 'capybara/rails'
+require 'capybara/rspec'
--- spec/features/send_sms_spec.rb
+++ spec/features/send_sms_spec.rb
@@ -1,25 +1,23 @@
 require 'spec_helper'
 require 'feature_helper'
 
-feature "Send SMS" do
-  before (:all) do
+describe "Send SMS", :type => :feature do
+  before (:each) do
     @message = FactoryGirl.build(:message)
     @text_message = @message.text_messages.build(firstname: "John", phone_number: "2185566465")
+    @client = Twilio::REST::Client.new(ENV["TEST_ACCOUNT_SID"], ENV["TEST_AUTH_TOKEN"])
+    @account = @client.accounts.get(ENV["TEST_ACCOUNT_SID"])
   end
 
   it "uploads and parses a CSV file" do
-    # @message = FactoryGirl.build(:message)
-    visit csv_upload_message_text_messages_url(:message_id =>@message.id)
-    click_button "Choose File"
-    select_file "../data/valid_csv.csv'"
+    visit "/messages/:message_id/text_messages/csv_upload"
+    page.attach_file("file_upload", "./spec/data/valid_csv.csv") 
     click_button "Import"
     current_path.should eq(import_confirmation_message_text_messages_url)
     page.should have_content("Phone numbers uploaded: 2")
   end
 
   it "integrate with Twilio's API to send a text message to all the phone numbers in that file" do
-    # @message = FactoryGirl.build(:message)
-    # @text_message = @message.text_messages.build(firstname: "John", phone_number: "612")
     visit import_confirmation_message_text_messages_url
     click_button "Send!"
     current_path.should eq(send_confirmation_message_text_messages_url)
@@ -27,36 +25,33 @@ feature "Send SMS" do
   end
 
   it "successfully sends the message" do
-      @client = Twilio::REST::Client.new(ENV["TEST_ACCOUNT_SID"], ENV["TEST_AUTH_TOKEN"])
-      @account = @client.accounts.get(ENV["TEST_ACCOUNT_SID"])
-      # @message = FactoryGirl.build(:message)
-      from = ENV["TWILIO_NUMBER"]
-      @message.text_messages.each do |text_message|
-        client.account.messages.create(
-          :from => from,
-          :to => text_message.phone_number.gsub(/^(\+1)|[^0-9]+/,''),
-          :body => "Hey #{text_message.firstname}, #{@message.content}"
-        )
-      status = @client.account.messages.list.status
-      expect(status).to eq("sent")
+    from = ENV["TWILIO_NUMBER"]
+    @message.text_messages.each do |text_message|
+      client.account.messages.create(
+        :from => from,
+        :to => text_message.phone_number.gsub(/^(\+1)|[^0-9]+/,''),
+        :body => "Hey #{text_message.firstname}, #{@message.content}"
+      )
+    end
+    status = @client.account.messages.list.status
+    expect(status).to eq("sent")
   end
 
   it "confirms all recipients received the message" do
     to = @client.account.messages.list.last.to
-    expect(to).to eq("+12185566465")
+    expect(to).to eq(@text_message.phone_number.gsub("+1", ""))
   end
 
   it "sends the message prefixed with the recipients first name." do
-    @message.body.should have_content("Hey John")
+    body = @client.account.messages.list.last.body
+    expect(body).to have_content("Hey John")
   end 
-
-  it "after successfully sending all the text messages, the app should show a 'success' screen."
 
   it "reports when messages have been sent" do
     visit import_confirmation_message_text_messages_url
     click_button "Send!"
     current_path.should eq(send_confirmation_message_text_messages_url)
-    page.should have_content("Done.")
+    page.should have_content("SENT")
   end
 
 end
